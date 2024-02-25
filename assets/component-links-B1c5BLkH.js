321b9680a554a19a35a9f03c9f7e8951c72f8bd7
GitHub Action <action@github.com>
2024-02-25 16:42:41 +0000

    deploy: 513ad68ab15b2d92e6096aaeb7e94bab2ce5368c

---
+++ assets/component-links-B1c5BLkH.js
@@ -0,0 +1,6 @@
+import{j as e}from"./index-C_B8nXvL.js";function s(t){const n={a:"a",li:"li",ul:"ul",...t.components};return e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"/posts/markdown-components-in-react",children:"React Components in Markdown"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://press-vue.servicestack.net/posts/markdown-components-in-vue",children:"Vue Components in Markdown"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://razor-ssg.web-templates.io/posts/javascript",children:".NET Razor SSG Vue Components in Markdown"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"https://blazor-vue.web-templates.io/posts/javascript",children:".NET Blazor Vue Components in Markdown"})}),`
+`]})}function r(t={}){const{wrapper:n}=t.components||{};return n?e.jsx(n,{...t,children:e.jsx(s,{...t})}):s(t)}export{r as default};
