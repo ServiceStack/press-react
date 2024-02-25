8b0a80ceeb82f90ef8557b4bf2f5a6cae1c0d084
GitHub Action <action@github.com>
2024-02-25 15:56:36 +0000

    deploy: c282ed1a0c5b5f7ca3eb02b6b01af9b6218f608a

--- assets/autogen-locode-DIifI9jo.js
+++
@@ -1,4 +0,0 @@
-import{j as e}from"./index-BXufVF9D.js";function s(t){const n={a:"a",p:"p",...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(n.p,{children:"In this video we show a step by step process for start with an existing database and customizations without the need for code first models."}),`
-`,e.jsxs(n.p,{children:[e.jsx(n.a,{href:"https://docs.servicestack.net/locode/",children:"Locode"}),` is a generated API driven by your APIs metadata.
-Combined with other high-level ServiceStack features, it becomes a way to rapidly develop web applications with robust service APIs
-that can be later extended with custom UIs.`]})]})}function a(t={}){const{wrapper:n}=t.components||{};return n?e.jsx(n,{...t,children:e.jsx(s,{...t})}):s(t)}export{a as default};
