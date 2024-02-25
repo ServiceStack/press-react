6f4ccdb11acc990328e78509b0d4ae3f5e81f380
Demis Bellot <demis.bellot@gmail.com>
2024-02-25 18:17:16 +0800

    Update SrcPage.tsx

--- src/components/SrcPage.tsx
+++ src/components/SrcPage.tsx
@@ -7,7 +7,7 @@ type Props = {
 }
 
 export default ({ path }:Props) => (
-    <SrcLink href={combinePaths('/example/react-press/src',path)}>
+    <SrcLink href={combinePaths('/src',path)}>
         <Icon icon="mdi:react" className="w-5 h-5 text-link-dark inline" />
     </SrcLink>
 )
