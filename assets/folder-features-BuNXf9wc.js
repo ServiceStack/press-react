bcea0f6d3cf12e34295d9422a13933853cab6dc8
GitHub Action <action@github.com>
2024-02-25 10:25:48 +0000

    deploy: 0451fb2099bb2dd0d163c7b5bd8baf42375fdf6b

--- assets/folder-features-BuNXf9wc.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./index-BkK4M1w9.js";function s(n){const e={files:"files",p:"p",...n.components};return t.jsx(e.files,{children:t.jsx(e.p,{children:`{
-_includes: {},
-_posts: {},
-_videos: {},
-_whatsnew: {},
-}`})})}function c(n={}){const{wrapper:e}=n.components||{};return e?t.jsx(e,{...n,children:t.jsx(s,{...n})}):s(n)}export{c as default};
