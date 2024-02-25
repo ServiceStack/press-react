d510460dd22b6ec08de192c71b6a02045686a1e9
GitHub Action <action@github.com>
2024-02-25 07:17:09 +0000

    deploy: c6cdb44b125e2e54e3384f0de3fbf59fcff4ddc3

---
+++ assets/feature1-BuCyQjRp.js
@@ -0,0 +1,7 @@
+import{j as t}from"./index-BjYFRgOq.js";function o(e){const n={p:"p",...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(n.p,{children:`Introducing OpenShuttle, the revolutionary new product that is set to change the space industry forever. OpenShuttle is a cutting-edge
+platform that provides detailed schematics for the very first rocket designed by me, which successfully made it to orbit.
+This incredible product is designed to give space enthusiasts and professionals alike access to the groundbreaking technology that
+was used to achieve this monumental feat.`}),`
+`,t.jsx(n.p,{children:`OpenShuttle is the result of years of hard work and dedication, and I am thrilled to finally be able to share it with the world.
+With its detailed schematics, OpenShuttle provides an unprecedented level of insight into the inner workings of the rocket,
+allowing users to explore the technology that made history.`})]})}function s(e={}){const{wrapper:n}=e.components||{};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o(e)}export{s as default};
