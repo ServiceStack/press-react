1616f0a4cd5a81e87f777d40884fa62aedd82e69
GitHub Action <action@github.com>
2024-02-26 06:46:46 +0000

    deploy: 0b9913add654bd7c7f97e684123ea63ad15c3d75

--- assets/nextjs-oVa29xYO.js
+++
@@ -1,4 +0,0 @@
-import{j as t}from"./index-BztdH8Fb.js";function i(e){const n={p:"p",...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(n.p,{children:`In this comprehensive walkthrough, we will guide you through the ServiceStack Next.js template, which offers an
-exceptional rapid development cycle when used with JetBrains Rider.`}),`
-`,t.jsx(n.p,{children:`This template is specifically designed for Microsoft .NET developers seeking a seamless and efficient Next.js React
-setup while employing the better performing Jamstack architecture.`})]})}function r(e={}){const{wrapper:n}=e.components||{};return n?t.jsx(n,{...e,children:t.jsx(i,{...e})}):i(e)}export{r as default};
