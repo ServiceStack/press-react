8e8f9223ce72d9c8517802f6a96383cc7d6bdc5e
GitHub Action <action@github.com>
2024-02-29 06:34:09 +0000

    deploy: c5d18f28af692cddf70736d4a536eec266598507

--- assets/autoquery-bookings-Ck7BmNvL.js
+++
@@ -1,8 +0,0 @@
-import{j as n}from"./index-COZ9WXnh.js";function i(e){const t={li:"li",p:"p",ul:"ul",...e.components};return n.jsxs(n.Fragment,{children:[n.jsx(t.p,{children:"From an empty web project, this video shows how to:"}),`
-`,n.jsxs(t.ul,{children:[`
-`,n.jsx(t.li,{children:"Add SQLite & Authentication support"}),`
-`,n.jsx(t.li,{children:"Define code-first Data Model with Validation"}),`
-`,n.jsx(t.li,{children:"Add User Management support"}),`
-`,n.jsx(t.li,{children:"Enable Audit History support"}),`
-`,n.jsx(t.li,{children:"Add Excel integration"}),`
-`]})]})}function r(e={}){const{wrapper:t}=e.components||{};return t?n.jsx(t,{...e,children:n.jsx(i,{...e})}):i(e)}export{r as default};
