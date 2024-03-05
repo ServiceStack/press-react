270a2ca241300f1f8781b6a225f0ddd0e6864924
GitHub Action <action@github.com>
2024-03-05 10:42:22 +0000

    deploy: 10bbff2fb77558e25338772d054e42ba263f5540

---
+++ assets/press-react-7SVBxEBR.js
@@ -0,0 +1,4 @@
+import{j as n}from"./index-BBUuWN-q.js";function o(e){const t={code:"code",p:"p",...e.components};return n.jsxs(t.p,{children:["In this video, we will show how you can use the ",n.jsx(t.code,{children:"press-react"}),` template to create a beautiful React and Markdown
+static blog or content centric Websites that's easy to manage and add content. It utilizes the vite-plugin-press library
+whose Markdown feature folders give you a consistent way of using markdown pages to create blogging, marketing landing
+pages and other content heavy websites.`]})}function s(e={}){const{wrapper:t}=e.components||{};return t?n.jsx(t,{...e,children:n.jsx(o,{...e})}):o(e)}export{s as default};
